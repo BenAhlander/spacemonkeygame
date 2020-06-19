7b4d0e69c7c1ec8de186ca57c2ef1ac089021e66
Ben Ahlander <ben.ahlander@gmail.com>
2020-06-18 18:21:38 -0600

    lower case image names

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -15,7 +15,7 @@ import FavoriteIcon from "@material-ui/icons/Favorite";
 import ShareIcon from "@material-ui/icons/Share";
 import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 import MoreVertIcon from "@material-ui/icons/MoreVert";
-import SpaceMonkeyIcon from "../../assets/icons/SpaceMonkeyLogoWhite.svg";
+import SpaceMonkeyIcon from "../../assets/icons/spaceMonkeyLogoWhite.svg";
 
 const useStyles = makeStyles((theme) => ({
   root: {
--- src/components/Nav/index.jsx
+++ src/components/Nav/index.jsx
@@ -6,7 +6,7 @@ import Typography from "@material-ui/core/Typography";
 import Button from "@material-ui/core/Button";
 import IconButton from "@material-ui/core/IconButton";
 import MenuIcon from "@material-ui/icons/Menu";
-import SpaceMonkeyIcon from "../../assets/icons/SpaceMonkeyLogoWhite.svg";
+import SpaceMonkeyIcon from "../../assets/icons/spaceMonkeyLogoWhite.svg";
 
 const useStyles = makeStyles((theme) => ({
   root: {
